091ddb481aa1521661e01f70fa84bed545d5cbfc
V.karthik <wrk.krthik@gmail.com>
2022-06-30 14:45:24 +0530

    boring stuff, like textures, not finished though !

---
+++ build/dependencies/glm/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/Repos/Kaine3DRenderingEngine/dependencies/glm
+# Build directory: D:/Repos/Kaine3DRenderingEngine/build/dependencies/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
---
+++ build/dependencies/glm/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Repos/Kaine3DRenderingEngine/dependencies/glm/glm
+# Build directory: D:/Repos/Kaine3DRenderingEngine/build/dependencies/glm/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
