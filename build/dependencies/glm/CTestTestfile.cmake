e17eeeddc4f6f83d87f50fb33483f1730bb15940
V.karthik <wrk.krthik@gmail.com>
2022-07-04 22:29:58 +0530

    boring stuff

--- build/dependencies/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Repos/Kaine3DRenderingEngine/dependencies/glm
-# Build directory: D:/Repos/Kaine3DRenderingEngine/build/dependencies/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- build/src/CMakeFiles/Kaine3DRenderer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Kaine3DRenderer.
-# This may be replaced when dependencies are built.
